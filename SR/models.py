f6fb0b5393fa4028a213725af08ac735c85a9b95
Desarrollador 08 <Desarrollo 08@grupocubo.ltda>
2017-06-28 15:12:12 -0500

    Cambios de estilo en codificación

--- SR/models.py
+++ SR/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from pydub import AudioSegment
 from django.conf import settings
 from os import listdir, remove
-from utils import WIT_API_KEY, GOOGLE_CLOUD_SPEECH_CREDENTIALS
+from . utils import WIT_API_KEY, GOOGLE_CLOUD_SPEECH_CREDENTIALS
 import speech_recognition as sr
 
 
@@ -14,7 +14,7 @@ class Speech_Recognition(models.Model):
     created_date = models.DateField(default=timezone.now)
 
     def recognition(self, audio=None):
-        if audio == None:
+        if audio is None:
             AUDIO_FILE = self.audioFile.path
         else:
             AUDIO_FILE = audio
@@ -22,33 +22,35 @@ class Speech_Recognition(models.Model):
         with sr.AudioFile(AUDIO_FILE) as source:
             audio = recognizer.record(source)
             try:
-                # temp_text= recognizer.recognize_google(audio, None, language="es-419")
-                temp_text= recognizer.recognize_wit(audio, key=WIT_API_KEY)
+                # temp_text = recognizer.recognize_google(audio, None, language="es-419")
+                temp_text = recognizer.recognize_wit(audio, key=WIT_API_KEY)
                 self.text += temp_text + " "
                 self.save()
             except sr.UnknownValueError:
                 print("No es posible entender el audio")
             except sr.RequestError as e:
                 print(
-                    "Could not request results from Google Speech Recognition service; {0}".format(e))
+                    "Could not request results from Google Speech Recognition service; \
+                    {0}".format(e))
 
     def analyze_audio(self):
         self.text = "test "
         AUDIO_FILE = self.audioFile.path
         file_in = AudioSegment.from_wav(AUDIO_FILE)
         if int(file_in.duration_seconds) > 5:
-            for a in range(0,int(file_in.duration_seconds),5):
+            for a in range(0, int(file_in.duration_seconds), 5):
                 b = a * 1000
-                fileOut = file_in[b:b+5000]
-                fileOut.export(settings.MEDIA_ROOT + "/temp/part"+str(a)+".wav", format = "wav")
-            for audio_part in listdir(settings.MEDIA_ROOT+"/temp/"):
-                self.recognition(settings.MEDIA_ROOT+"/temp/"+audio_part)
+                fileOut = file_in[b:b + 5000]
+                fileOut.export(settings.MEDIA_ROOT +
+                               "/temp/part" + str(a) + ".wav", format="wav")
+            for audio_part in listdir(settings.MEDIA_ROOT + "/temp/"):
+                self.recognition(settings.MEDIA_ROOT + "/temp/" + audio_part)
         else:
             self.recognition()
 
-        if len(listdir(settings.MEDIA_ROOT+"/temp/")) > 0:
-            for temp_file in listdir(settings.MEDIA_ROOT+"/temp/"):
-                remove(settings.MEDIA_ROOT+"/temp/"+temp_file)
+        if len(listdir(settings.MEDIA_ROOT + "/temp/")) > 0:
+            for temp_file in listdir(settings.MEDIA_ROOT + "/temp/"):
+                remove(settings.MEDIA_ROOT + "/temp/" + temp_file)
 
     def __str__(self):
         return self.description + " - " + str(self.created_date)
--- SR/views.py
+++ SR/views.py
@@ -15,14 +15,17 @@ def sr_list(request):
     SR = Speech_Recognition.objects.order_by('-created_date')
     return render(request, 'SR/sr_list.html', {'SR': SR, 'form': new_sr})
 
+
 def sr_detail(request, item_id):
     item = get_object_or_404(Speech_Recognition, pk=item_id)
     return render(request, 'SR/sr_detail.html', {'item': item})
+
 
 def analyze_audio(request, item_id):
     item = get_object_or_404(Speech_Recognition, pk=item_id)
     item.analyze_audio()
     return render(request, 'SR/sr_detail.html', {'item': item})
+
 
 def sr_remove(request, item_id):
     item = get_object_or_404(Speech_Recognition, pk=item_id)
