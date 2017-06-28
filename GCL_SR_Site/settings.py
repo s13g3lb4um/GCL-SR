26c25cc48f1eec5d7ffeb15acfb429e1c85d2bd4
Desarrollador 08 <Desarrollo 08@grupocubo.ltda>
2017-06-28 13:48:37 -0500

    Se cambia configuración de seguridad para producción

--- GCL_SR_Site/settings.py
+++ GCL_SR_Site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from .utils import DJANGO_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'awzpboo!f@enurr+#@u!r(pscm4s@01_o^qrwavb0#e^5$uzf3'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ GCL_SR_Site/utils.py
@@ -0,0 +1,1 @@
+DJANGO_KEY = 'awzpboo!f@enurr+#@u!r(pscm4s@01_o^qrwavb0#e^5$uzf3' 
