f5e59cbad71aca1d663e3e4317c928da82c71c13
s13g3lb4um <s13g3lb4um@gmail.com>
2017-07-08 04:09:34 -0500

    Cambios menores para producción

--- GCL_SR_Site/settings.py
+++ GCL_SR_Site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from . utils import DJANGO_KEY, HOST
+from . utils import DJANGO_KEY, HOST, DEBUG_MODE
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG_MODE
 
 ALLOWED_HOSTS = HOST
 
