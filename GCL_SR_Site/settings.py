93bddd6848a227c33c7bf7cf38734ff064e66912
s13g3lb4um <s13g3lb4um@gmail.com>
2017-07-14 20:35:19 -0500

    Arreglado la visualizaciòn de estilos

--- GCL_SR_Site/settings.py
+++ GCL_SR_Site/settings.py
@@ -74,29 +74,12 @@ WSGI_APPLICATION = 'GCL_SR_Site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-
-if DEBUG_MODE:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        # 'default': {
-        #     'ENGINE': 'django.db.backends.mysql',
-        #     'OPTIONS': {
-        #         'read_default_file': './my.cnf',
-        #         },
-        # }
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
